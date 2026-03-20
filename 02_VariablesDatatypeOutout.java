5cb43f605ec62512cfce8fd8c013f69691d84dde
iamamar-codes <kushwahaamar104@gmail.com>
2026-03-20 18:59:43 +0530

    Java Basics (Variables + Data Types + Output)

---
+++ 02_VariablesDatatypeOutout.java
@@ -0,0 +1,13 @@
+class VariablesDatatypeOutout {
+    public static void main(String[] args){
+       String Name = "Amar Kushwaha";
+       int Age = 23;
+       char grad = 'A';
+       double height = 5.7;
+
+       System.out.println("Name: "+ Name);
+       System.out.println("Age: "+ 23);
+       System.out.println("Grad: "+ grad);
+       System.out.println("Height: "+ height);
+    }
+}
