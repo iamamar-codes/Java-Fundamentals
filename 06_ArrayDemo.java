36c4f21c18078548af705564db24ca5d5f0ac991
iamamar-codes <kushwahaamar104@gmail.com>
2026-03-22 21:32:59 +0530

    Array Demo Program

---
+++ 06_ArrayDemo.java
@@ -0,0 +1,15 @@
+class ArrayDemo {
+    public static void main(String[] args){
+          int[] arr = {4, 6, 7, 5, 8, 10};
+
+          System.out.println(arr[0]);
+          System.out.println(arr[1]);
+          System.out.println(arr[2]);
+          System.out.println(arr[3]);
+          System.out.println(arr[4]);
+          System.out.println(arr[5]);
+          System.out.println(arr[6]);
+
+
+    }
+}
