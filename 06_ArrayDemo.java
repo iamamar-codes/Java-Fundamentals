bc9e9925c8d940e930414fbbaab552e8bb2be8bf
iamamar-codes <kushwahaamar104@gmail.com>
2026-03-23 21:30:14 +0530

    Another Method for Creating Array

--- 06_ArrayDemo.java
+++ 06_ArrayDemo.java
@@ -1,6 +1,7 @@
 class ArrayDemo {
     public static void main(String[] args){
-          int[] arr = {4, 6, 7, 5, 8, 10};
+        //Mrthod 1- for initilize array
+          int[] arr = {4, 6, 7, 5, 8, 2};
 
           System.out.println(arr[0]);
           System.out.println(arr[1]);
@@ -8,8 +9,19 @@ class ArrayDemo {
           System.out.println(arr[3]);
           System.out.println(arr[4]);
           System.out.println(arr[5]);
-          System.out.println(arr[6]);
+          
 
+        //Method 2- for initilize array
+        int[] arr1 = new int [5];
+          arr1[0] = 10;
+          arr1[1] = 20;
+          arr1[2] = 30;
+          arr1[3] = 40;
+          arr1[4] = 50;
+
+        for(int i=0; i<arr1.length; i++){
+            System.out.println(arr1[i]);
+        }
 
     }
 }
