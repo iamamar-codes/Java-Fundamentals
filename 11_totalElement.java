6e72c4072db320e26252d8315bc5a0ba9c5fd60e
iamamar-codes <kushwahaamar104@gmail.com>
2026-03-24 19:51:13 +0530

    Count total number of elements

---
+++ 11_totalElement.java
@@ -0,0 +1,6 @@
+class totalElement {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 33, 54, 65, 76};
+        System.out.println("Total number of elements: "+ a.length);
+    }
+}
