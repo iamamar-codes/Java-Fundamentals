16fa94e6d818addd0d9f110142f0b6c52d96ee7c
iamamar-codes <kushwahaamar104@gmail.com>
2026-03-25 20:58:49 +0530

    Count even and odd numbers

---
+++ 14_EvenOddArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class EvenOddArray {
+    public static void main(String[] args) {
+        int size, i;
+        int evenCount = 0;
+        int oddCount = 0;
+        Scanner sc = new Scanner(System.in);
+
+        //Take size of Array
+        System.out.print("Enter Array Size: ");
+        size = sc.nextInt();
+        int arr[] = new int[size];
+
+        //Take array elements
+        System.out.print("Enter element: ");
+        for(i=0; i<size; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        
+        //Count even and odd
+        for(int elements: arr){
+            if(elements % 2 == 0){
+                evenCount++;
+            }else{
+                oddCount++;
+            }
+        }
+
+        System.out.println("Even = " + evenCount);
+        System.out.println("odd = " + oddCount);
+        
+    }
+}
