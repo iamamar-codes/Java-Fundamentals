18e18790df2ac8cfae2696042353171dbe0d418a
iamamar-codes <kushwahaamar104@gmail.com>
2026-03-31 20:45:26 +0530

    Basic java Program for understanding structure of program

---
+++ DSA-JAVA/06_Basics.java
@@ -0,0 +1,10 @@
+class basics {
+    public static void main(String[] args) {
+
+        System.out.println("kushwaha" + " Amar");
+        System.out.println("4+3");
+        System.out.println(4 + 3);
+
+    }
+
+}
